7a110bc2d87a9b5e445a1bd7227921280692a668
SSAS Admin <ssasadmin@pertamina0.onmicrosoft.com>
2022-06-06 10:52:39 +0700

    "commit"

---
+++ Models/PromotionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectPromotion.Models
+{
+    public class PromotionViewModel
+    {
+        public Promotion Promotion { get; set; }
+        public IEnumerable<Stores> StoresViewModel { get; set; }
+    }
+}
